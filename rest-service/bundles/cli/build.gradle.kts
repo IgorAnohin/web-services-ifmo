e8d640383d6289c581e935d249466ceda09a467b
Igor Anokhin <igr.anokhin@gmail.com>
2022-05-30 02:48:13 +0300

    fix cli bund deps

--- rest-service/bundles/cli/build.gradle.kts
+++ rest-service/bundles/cli/build.gradle.kts
@@ -7,9 +7,14 @@ application {
 }
 
 dependencies {
+    api(libs.kotlinLogging.jvm)
+    api(libs.log4j.api)
+
     implementation(projects.restService.client)
     implementation(libs.clikt)
     implementation(libs.mordant)
 
     implementation(libs.kotlin.reflect)
+    implementation(libs.log4j.core)
+    implementation(libs.log4j.slf4jImpl)
 }
